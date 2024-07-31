9b1c0111d2c697af398b67ec4bfe1be18d76942d
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-07-31 16:23:00 +0200

    test,feat: tests for complex values

--- src/neuralil/spherical_bessel/test_functions.py
+++ src/neuralil/spherical_bessel/test_functions.py
@@ -11,8 +11,11 @@ from .functions import create_j_l, _j_0, _j_1
 
 MAX_TEST_ORDER = 40
 TEST_ORDERS = [i for i in range(MAX_TEST_ORDER)]
+MAX_RADIUS = 50.0
 
-r = jnp.linspace(1e-3, 50., num=1001)
+r = jnp.linspace(1e-3, MAX_RADIUS, num=1001)
+z = jnp.mgrid[1e-3:MAX_RADIUS:0.1, 1e-3:MAX_RADIUS:0.1]
+z = -z[0] + 1j * z[1]
 
 # Test _j_0 function
 def test_j_0():
@@ -33,13 +36,13 @@ def test_create_j_l_invalid_order():
 ## Test for real valued arguments
 
 @pytest.mark.parametrize("order", TEST_ORDERS)
-def test_create_j_l_single_order(order):
+def test_real_create_j_l_single_order(order):
     func = create_j_l(order=order, dtype=jnp.float64, output_all=False)
     expected = spherical_jn(order, r)
     assert np.allclose(func(r), expected, atol=1e-8, rtol=1e-8)
 
 
-def test_create_j_l_output_all():
+def test_real_create_j_l_output_all():
     func = create_j_l(order=MAX_TEST_ORDER, dtype=jnp.float64, output_all=True)
     expected = []
     for order in range(MAX_TEST_ORDER+1):
@@ -47,3 +50,22 @@ def test_create_j_l_output_all():
     expected = np.array(expected).swapaxes(0, 1)
     result = func(r)
     assert np.allclose(func(r), expected, atol=1e-8, rtol=1e-8)
+
+
+## Test for complex valued arguments
+
+@pytest.mark.parametrize("order", TEST_ORDERS)
+def test_complex_create_j_l_single_order(order):
+    func = create_j_l(order=order, dtype=jnp.complex128, output_all=False)
+    expected = spherical_jn(order, z)
+    assert np.allclose(func(z), expected, atol=1e-8, rtol=1e-8)
+
+
+def test_complex_create_j_l_output_all():
+    func = create_j_l(order=MAX_TEST_ORDER, dtype=jnp.complex128, output_all=True)
+    expected = []
+    for order in range(MAX_TEST_ORDER+1):
+        expected.append(spherical_jn(order, z))
+    expected = np.array(expected).swapaxes(0, 1)
+    result = func(z)
+    assert np.allclose(func(z), expected, atol=1e-8, rtol=1e-8)
