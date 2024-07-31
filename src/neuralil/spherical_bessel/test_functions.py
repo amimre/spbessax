2872c5e16ebcf5b990758a2600c2238d2fbfb2dc
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-07-31 16:06:14 +0200

    feat: add tests for real valued jn

--- src/neuralil/spherical_bessel/test_functions.py
+++ src/neuralil/spherical_bessel/test_functions.py
@@ -1,8 +1,13 @@
 import pytest
 import jax.numpy as jnp
 from jax import grad
+from jax import config
+config.update("jax_enable_x64", True)
 import numpy as np
-from .functions import create_j_l, _j_0, _j_1, _calc_starting_order_Cai
+
+from scipy.special import spherical_jn
+
+from .functions import create_j_l, _j_0, _j_1
 
 MAX_TEST_ORDER = 40
 TEST_ORDERS = [i for i in range(MAX_TEST_ORDER)]
@@ -24,3 +29,23 @@ def test_create_j_l_invalid_order():
     with pytest.raises(ValueError):
         create_j_l(-1)
 
+
+## Test for real valued arguments
+
+@pytest.mark.parametrize("order", TEST_ORDERS)
+def test_create_j_l_single_order(order):
+    func = create_j_l(order=order, dtype=jnp.float64, output_all=False)
+    print('HERE', func(r), flush=True)
+    expected = spherical_jn(order, r)
+    assert np.allclose(func(r), expected, atol=1e-8, rtol=1e-8)
+
+
+def test_create_j_l_output_all():
+    func = create_j_l(order=MAX_TEST_ORDER, dtype=jnp.float64, output_all=True)
+    expected = []
+    for order in range(MAX_TEST_ORDER+1):
+        expected.append(spherical_jn(order, r))
+    expected = np.array(expected).swapaxes(0, 1)
+    result = func(r)
+    print(jnp.max(result-expected), jnp.nanmax(result-expected))
+    assert np.allclose(func(r), expected, atol=1e-8, rtol=1e-8)
